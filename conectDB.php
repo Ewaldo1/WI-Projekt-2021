ef34b1e200c18bfddc1e31f46ad321247c683564
EWALDO FISCHER <ewaldo.fischer-alpire@informatik.hs-fulda.degit config --global user.name EWALDO>
2021-11-27 10:40:47 +0100

    DB wurdet bei Regist erstellt

--- conectDB.php
+++ conectDB.php
@@ -1,12 +1,15 @@
 <?php
 
 
-    $_host = "locahost";
-    $_user = "root";
-    $_passwordDB = "";
-    $_datenBank = "ewaldo"; //wir müssen noch uns Eignen für eine Databank
+    //Hier wir Verbinden wir uns am die Datenbank
 
-    $_myServer = mysqli_connect($_host, $_user, $_passwordDB, $_datenBank);
+    $_conex = new mysqli("localhost", "root", "root", "dbproject");
 
+ /*   if($_conex) {
+
+        echo "conectado ";
+    } else {
+        echo "dont conect <br\n>";
+    } */
 
 ?>
--- regist.php
+++ regist.php
@@ -9,16 +9,15 @@
     gib deine Email-Adresse:<br>
     <input name="email" size=16><br>
     gib deine Pasword:<br>
-    <input name="password" size=12><br>
+    <input name="password" size=10><br>
     gib Alter<br>
-    <input name="old" size=8><br>
+    <input name="old" size=5><br>
     gib deine User-Name<br>
-    <input name="userName" size=1><br>
+    <input name="userName" size=10><br>
     <br>
     <input type="submit" name = "submit" value="Anmeldung" >
 
 </form>
-
 
 
 </body>
@@ -26,19 +25,56 @@
 
 <?php
 
-    require "WebShop/FileProbe.php";
+    require "conectDB.php";
 
     $_userEmail = $_POST["email"];
     $_userPassword = $_POST["password"];
     $_userOld = $_POST["old"];
     $_userName = $_POST["userName"];
 
-   // $insertar = "INSERT INTO Rigister(email, password, name, geburtsDatum) VALUES ('$_email', '$_password', '$_name', '$_old')";
-//REVISAR
+    //HIER WIRD ÜBERPRÜFT ob gab RICHTIGE ABGABE bei EMAIL UND ALTER
+
+    $prüfung1 = verificationData($_userEmail, $_userOld);
 
 
 
+    //Die Tabelle BENUTZER Enthählt info von unsere Users!
 
+    $insert = "INSERT INTO benutzer(email, password, geburtsDatum, UserName)VALUES ('$_userEmail', '$_userPassword', '$_userOld', '$_userName')";
+    $result = mysqli_query($_conex, $insert);
+
+
+    //TODO $_CONEX UND $_INSERT sollen in der Funktion verifikationDB() integriert werden
+
+    if($result) {
+        echo "Sind Sie jetzt registiert";
+    } else{
+        var_dump($result);
+        echo "Fehler bei Regestrierung";
+    }
+
+
+
+    function verificationData($_mailP, $_oldP) {
+
+        if(!filter_var($_oldP, FILTER_VALIDATE_INT)) {
+            echo "Geben Sie bitte gültige Alter <br />\n";
+            return false;
+        }
+        if(!filter_var($_mailP, FILTER_VALIDATE_EMAIL)) {
+            echo "Geben sie ein gültige Email bitteee! <br />\n";
+            return false;
+        }
+
+    }
+
+    function verrificationDataDB ($prüfung1) {
+        if ($prüfung1 == false) {
+            return 0;
+        }
+
+
+    }
 
 
 
