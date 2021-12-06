d1e79ac13b56b55fc257866e517264e6bcf62b93
Huong Duong <diem-huong.duong@informatik.hs-fulda.de>
2021-12-06 15:35:12 +0100

    houng

--- Index.php
+++ Index.php
@@ -37,7 +37,7 @@ if(isset($_POST["submit"])){
             header("Location: startseite.php");
     } else {
         echo "Username oder Passwort falsch!";
-    }
+    } //bla bla
 }
 ?>
 <section class="container">
