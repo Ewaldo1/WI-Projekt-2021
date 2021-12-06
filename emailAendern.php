0e2802a25efb55b784cd0207a6c925c06c2bed28
EWALDO FISCHER <ewaldo.fischer-alpire@informatik.hs-fulda.degit config --global user.name EWALDO>
2021-12-06 14:47:03 +0100

    wir können jz email auch ändern

--- emailAendern.php
+++ emailAendern.php
@@ -40,5 +40,53 @@ $password = $_POST["passwort"];
 $alteEmail = $_POST["oldEmail"];
 $neueEmail = $_POST["newEmail"];
 
+
+if(isset($_POST['submit'])) {
+
+    //Hier wird überprüft ob die Neue Password richtig zweimal eingegeben wurdet
+
+
+
+//Suche Option muss noch optimiert werden
+
+    $sql = "SELECT Email FROM nutzer WHERE '" . $alteEmail . "' = Email";
+    $searchResult = mysqli_query($con,$sql);
+
+    if(mysqli_num_rows($searchResult) <= 0) {
+        echo "Falsche Email eingeben";
+
+    } else {
+        echo "Rictige Email <br/n>";
+        setNewEmail($alteEmail, $neueEmail, $con);
+    }
+
+
+  
+
+}
+
+
+
+function setNewEmail($givemeOldE, $givemeNewE, $givemeCon) {
+    //    UPDATE `nutzer` SET `Email`=[value-1],`Passwort`=[value-2],`Geburtsdatum`=[value-3],`Username`=[value-4] WHERE 1
+
+    $upDateThis = "UPDATE nutzer SET Email = '$givemeNewE' WHERE Email =  '".$givemeOldE."'  ";
+
+
+    if($upDateThis) {
+        echo "you are online! <br/n>";
+    }
+    $result2 = mysqli_query($givemeCon, $upDateThis);
+
+    if($result2) {
+        echo "You update this";
+    } else {
+        echo "Problem!!";
+    }
+
+
+}
+
+
 //Hier muss ich noch schauen wie ich Seile in Datenbanken suchen kann und dann was ändern..
 //Password wird nur auf sichere Grunde gefragt!
